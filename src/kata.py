122e31d0c84665e85de33f3a018c00a61de35d4c
Travis Reyburn <treyburn@alteryx.com>
2020-08-31 11:27:35 -0600

    filled in function logic. unit tests pass

--- src/kata.py
+++ src/kata.py
@@ -2,4 +2,7 @@ class Kata:
 
     @staticmethod
     def reverse_str(src_str: str) -> str:
-        pass
+        rev_str = ""
+        for letter in src_str:
+            rev_str = letter + rev_str
+        return rev_str
