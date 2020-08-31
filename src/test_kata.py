ac9b1ef6170bd44bf8dae0f61019a26ba7d13bf5
Travis Reyburn <treyburn@alteryx.com>
2020-08-31 11:36:40 -0600

    added additional unit test for bad types

--- src/test_kata.py
+++ src/test_kata.py
@@ -3,8 +3,13 @@ from src.kata import Kata
 
 
 class TestKata:
-    reverse_str_test_data = [["abc", "cba"], ["step on no pets", "step on no pets"], ["travis", "sivart"]]
+    valid_rev_str_data = [["abc", "cba"], ["step on no pets", "step on no pets"], ["travis", "sivart"]]
+    invalid_rev_str_data = [14, [], True]
 
-    @pytest.mark.parametrize("src_str, expect_str", reverse_str_test_data)
-    def test_reverse_str(self, src_str: str, expect_str: str):
-        assert expect_str == Kata.reverse_str(src_str)
+    @pytest.mark.parametrize("valid_str, expect_str", valid_rev_str_data)
+    def test_reverse_str_valid(self, valid_str: str, expect_str: str):
+        assert expect_str == Kata.reverse_str(valid_str)
+
+    @pytest.mark.parametrize("invalid_str", invalid_rev_str_data)
+    def test_reverse_str_invalid(self, invalid_str: any):
+        assert not Kata.reverse_str(invalid_str)
