9955337d1edd2dce59d64396dc0ab913e34508d3
Travis Reyburn <treyburn@alteryx.com>
2020-08-31 11:36:59 -0600

    added logic to handle bad types

--- src/kata.py
+++ src/kata.py
@@ -3,6 +3,8 @@ class Kata:
     @staticmethod
     def reverse_str(src_str: str) -> str:
         rev_str = ""
+        if not isinstance(src_str, str):
+            return rev_str
         for letter in src_str:
             rev_str = letter + rev_str
         return rev_str
